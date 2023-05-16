c5b8504712ec665717889e2b13529e303e1da79c
D Shubhakarini CIJD06 <SAH000895@tmfhyd.local>
2023-05-16 15:47:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -11,52 +11,52 @@ import{ BrowserRouter as Router, Routes,Route,Link} from "react-router-dom";
 function App() {
   return (
     <Router>
-      <nav class="navbar navbar-expand-lg bg-danger">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="fa fa-envelope"></i>Shubhakarnidoulthabad@gmail.com</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-danger">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="#"><i className="fa fa-envelope"></i>Shubhakarnidoulthabad@gmail.com</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link activee" aria-current="page" href="#">+91 123-456-7890</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link activee" aria-current="page" to="#">+91 123-456-7890</Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Secunderabad</a>
+        <li className="nav-item">
+          <Link className="nav-link" to="#">Secunderabad</Link>
         </li>
        
         
       </ul>
-      <form class="d-flex" className="Shubha" role="search">
-      <i class="fa fa-instagram"></i>
-      <i class="fa fa-twitter"></i>
-      <i class="fa fa-facebook"></i>
-      <i class="fa fa-youtube"></i>
+      <form className="d-flex" classNameName="Shubha" role="search">
+      <i className="fa fa-instagram"></i>
+      <i className="fa fa-twitter"></i>
+      <i className="fa fa-facebook"></i>
+      <i className="fa fa-youtube"></i>
       </form>
     </div>
   </div>
 </nav>
-      <div class="App">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary p-2">
-  <div class="container-fluid">
-    <Link class="navbar-brandd" to="/Slider">APNA BAWARCHI</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <div className="App">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary p-2 sticky-top">
+  <div className="container-fluid">
+    <Link className="navbar-brandd" to="/Slider">APNA BAWARCHI</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/Slider"><i class="fa fa-home "></i> Home</Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/Slider"><i className="fa fa-home "></i> Home</Link>
         </li>
-        <li class="nav-item">
-        <Link class="nav-link active" aria-current="page" to="/About"><i class="fa fa-question-circle"></i> About Us</Link>
+        <li className="nav-item">
+        <Link className="nav-link active" aria-current="page" to="/About"><i className="fa fa-question-circle"></i> About Us</Link>
         </li>
-        <li class="nav-item">
-        <Link class="nav-link active" aria-current="page" to="/contact"><i class="fa fa-phone"></i> Contact Us</Link>
+        <li className="nav-item">
+        <Link className="nav-link active" aria-current="page" to="/contact"><i className="fa fa-phone"></i> Contact Us</Link>
         </li>
-        <li class="nav-item">
-        <Link class="nav-link active" aria-current="page" to="/Menu"><i class="fa fa-bars"></i> Menu</Link>
+        <li className="nav-item">
+        <Link className="nav-link active" aria-current="page" to="/Menu"><i className="fa fa-bars"></i> Menu</Link>
         </li>
       </ul>
       
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -12,21 +12,21 @@ export default function Slider() {
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active ">
-      <img src="https://wallpapercave.com/wp/wp1874159.jpg" class="d-block w-100" alt="..."/>
+      <img src="https://wallpapercave.com/wp/wp1874159.jpg" class="d-block w-100 sliderimg" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h1 class="btn">APNA BAWARCHI RESTAURANT<span></span></h1>
         <p><h4 class="btn">Live, love, eat</h4><span></span> </p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://assets.gqindia.com/photos/5cdc7489956b1c5a8f6514ab/16:9/w_1280,c_limit/cafe-mozaic.png" class="d-block w-100" alt="..."/>
+      <img src="https://assets.gqindia.com/photos/5cdc7489956b1c5a8f6514ab/16:9/w_1280,c_limit/cafe-mozaic.png" class="d-block w-100 sliderimg" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h1 class="btn">ENJOY OUR DELICIOUS FOOD<span></span></h1>
         <p><h4 class="btn">Try Something New</h4><span></span></p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://wallup.net/wp-content/uploads/2019/09/761345-restaurant-food-architecture-interior-design-room.jpg" class="d-block w-100" alt="..."/>
+      <img src="https://wallup.net/wp-content/uploads/2019/09/761345-restaurant-food-architecture-interior-design-room.jpg" class="d-block w-100 sliderimg" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h1 class="btn">HURRY AND BOOK YOUR TABLE NOW<span></span></h1>
         <p><h4 class="btn">Come and Enjoy with your friends and family</h4><span></span></p>
